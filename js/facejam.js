94d4e41e7466d4b8ccdb8013bbd97d580d311bd1
Chris Johnson <pointninerepeating@gmail.com>
2014-03-22 19:06:13 -0700

    added inputs for specifying shape and number of cells to render

--- js/facejam.js
+++ js/facejam.js
@@ -1,34 +1,21 @@
 (function() {
-  //var imageDrop = document.getElementById("image-drop");
-
+  var shapeSelect = document.querySelectorAll('select[name="shape"]')[0];
+  var cellInput = document.querySelectorAll('input[name="cells"]')[0];
   var canvas = document.getElementsByTagName("canvas")[0];
   var ctx = canvas.getContext('2d');
   var img = new Image();
 
-  var N = 50;
-
   img.onload = function() {
+    var N = parseInt(cellInput.value);
+    var shape = shapeSelect.options[shapeSelect.selectedIndex].value;
     var aspectRatio = img.width / img.height;
     var width = canvas.width = canvas.height * aspectRatio;
     var height = canvas.height;
-    //ctx.drawImage(img, 0, 0, img.width / 2, img.height/2);
-    ctx.drawImage(img, 0, 0, width, height);
-    console.log(img.width, img.height);
-    //var imageData = ctx.getImageData(0, 0, img.width, img.height / 2);
-    //var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-    //var yPoints = canvas.height / N;
-    //var xPoints = yPoints;
-
     var patchWidth = width / N;
     var patchHeight = height / N;
-
-    console.log('width', width, 'height', height);
-    console.log('patch width', patchWidth, 'patch height', patchHeight);
-
     var facePoints = [];
 
-    //console.log("image data length", imageData.data.length);
+    ctx.drawImage(img, 0, 0, width, height);
 
     for(var x=0; x < N; x++) {
       for(var y=0; y < N; y++) {
@@ -38,17 +25,15 @@
 	  patchWidth,
 	  patchHeight
 	  )
-
+	var count = 0;
 	var length = patch.data.length;
-	var i = -4;
 	var rgb = {
 	  r: 0,
 	  g: 0,
 	  b: 0
 	};
-	
-	var count = 0;
-	
+
+	var i = -4;
 	while ( (i+=4) < length) {
 	  count++;
 	  rgb.r += patch.data[i];
@@ -56,8 +41,6 @@
 	  rgb.b += patch.data[i+2];
 	}
 
-	//console.log('count', count);
-	
 	rgb.r = ~~(rgb.r / count);
 	rgb.g = ~~(rgb.g / count);
 	rgb.b = ~~(rgb.b / count);
@@ -65,32 +48,23 @@
 	rgb.y = y;
 
 	facePoints.push(rgb);
-
       }
     }
 
-    console.log('loop done');
-    console.log(facePoints);
-    inYourFace(facePoints, width, height);
-
+    inYourFace(facePoints, width, height, shape);
+    canvas.style.display = 'none';
   };
 
   canvas.ondragover = function () { this.className = 'hover'; return false; };
   canvas.ondragend = function () { this.className = ''; return false; };
   canvas.ondrop = function (e) {
-    //this.className = '';
     e.preventDefault();
-
     var file = e.dataTransfer.files[0],
     reader = new FileReader();
     reader.onload = function (event) {
-      console.log(event.target);
       img.src = event.target.result;
-      //canvas.style.background = 'url(' + event.target.result + ') no-repeat center';
     };
-    console.log(file);
     reader.readAsDataURL(file);
-
     return false;
   };
 
--- js/inyourface.js
+++ js/inyourface.js
@@ -1,6 +1,6 @@
 (function() {
 
-var inYourFace = function(data, width, height) {
+var inYourFace = function(data, width, height, shape) {
   var cellWidth = width / Math.sqrt(data.length);
   var cellHeight = height / Math.sqrt(data.length);
 
@@ -10,13 +10,16 @@ var inYourFace = function(data, width, height) {
     attr("height", height).
     append("g");
 
-  svg.selectAll("rect").
+  svg.selectAll(shape).
     data(data).
-    enter().append("rect").
+    enter().append(shape).
     attr('width', cellWidth).
     attr('height', cellHeight).
     attr('x', function(d, i) { return d.x * cellWidth; }).
     attr('y', function(d, i) { return d.y * cellHeight; }).
+    attr('r', Math.max(cellWidth, cellHeight) / 2).
+    attr('cx', function(d, i) { return d.x * cellWidth + cellWidth / 2; }).
+    attr('cy', function(d, i) { return d.y * cellHeight + cellHeight / 2; }).
     attr('fill', function(d) { return 'rgb(' + d.r + ',' +  d.g + ',' + d.b + ')'; });
 };
 
